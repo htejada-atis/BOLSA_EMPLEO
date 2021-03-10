96bb7d52bbc197bbe94b7a088b7fe924834bde32
javier <javilpx2.jl@gmail.com>
2021-03-10 12:47:48 +0100

    agregadas tablas noticias y triggers

---
+++ Documentos/scripts/opc.bolsaempleo/datos_desarrollo/clean/05-eje-noticias.sql
@@ -0,0 +1,7 @@
+DROP TABLE TBEP_HTO_NOTICIAS;
+--/////////////////////
+DROP TABLE TBEP_NOTICIAS;
+--/////////////////////
+DROP SEQUENCE QBEP_NOT;
+--/////////////////////
+DROP SEQUENCE QBEP_HTO_NOT;
