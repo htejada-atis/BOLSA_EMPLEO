9dc1fd90773af5de3897e382409190d2973f0f9b
Juan Ramón Moral <jmoral@ujaen.es>
2021-03-17 11:47:41 +0100

    libreria para json

--- test/bbdd/UtilsTestDocentia.java
+++ test/bbdd/UtilsTestDocentia.java
@@ -25,7 +25,7 @@ public class UtilsTestDocentia {
 		BbddRunner.ejecutar("Documentos/scripts/opc.docentia/datos_desarrollo/dropTables.sql");
 		BbddRunner.ejecutar("Documentos/scripts/opc.docentia/creartablasdocentia.sql");
 		BbddRunner.ejecutar("Documentos/scripts/opc.docentia/creacionsecuenciasytriggers.sql");
-		BbddRunner.insertMasivo("Documentos/scripts/opc.docentia/datos_desarrollo/creacionregistrosprueba.sql");    	
+		BbddRunner.insertMasivo("Documentos/scripts/opc.docentia/datos_desarrollo/creacionregistrosprueba.sql");
 	}
 	
     /** obtiene una peticion autenticada con el usuario de pruebas de docentia.
