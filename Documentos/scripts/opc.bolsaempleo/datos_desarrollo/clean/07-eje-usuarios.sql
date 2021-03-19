df751e5d6799ba980e138d9c15d5df4ee195ef14
Benito Rodríguez <brodriguez@atisoluciones.com>
2021-03-19 07:51:49 +0100

    Inicio de tablas

---
+++ Documentos/scripts/opc.bolsaempleo/datos_desarrollo/clean/07-eje-usuarios.sql
@@ -0,0 +1,7 @@
+DROP TABLE TBEP_HTO_USUARIOS;
+--/////////////////////
+DROP TABLE TBEP_USUARIOS;
+--/////////////////////
+DROP SEQUENCE QBEP_USU;
+--/////////////////////
+DROP SEQUENCE QBEP_HTO_USU;
