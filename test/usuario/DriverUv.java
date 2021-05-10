404f70454dab2fddca11a1e79e63921cf66ad3fb
Juan Ramón Moral <jmoral@ujaen.es>
2021-05-10 10:23:00 +0200

    display 99

--- test/usuario/DriverUv.java
+++ test/usuario/DriverUv.java
@@ -23,7 +23,7 @@ public class DriverUv {
 	public static void inicializaDriver() {
 		System.setProperty("webdriver.gecko.driver", "Documentos/selenium/drivers/geckodriver");
 		FirefoxOptions firefoxOptions = new FirefoxOptions();
-		firefoxOptions.addArguments("--display=0");
+		firefoxOptions.addArguments("--display=99");
 		driver = new FirefoxDriver(firefoxOptions);
 		driver.manage().timeouts().pageLoadTimeout(TIEMPO_MAXIMO_ESPERA, TimeUnit.SECONDS);
 	}
