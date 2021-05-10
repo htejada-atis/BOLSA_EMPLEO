d8da405fb1053e3ce2ec1a8b201a0412a15eb889
Juan Ramón Moral <jmoral@ujaen.es>
2021-05-10 11:13:41 +0200

    captura de pantalla inicial

--- test/usuario/DriverUv.java
+++ test/usuario/DriverUv.java
@@ -1,10 +1,15 @@
 package usuario;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,7 +29,6 @@ public class DriverUv {
 		System.setProperty("webdriver.gecko.driver", "Documentos/selenium/drivers/geckodriver");
 		FirefoxOptions firefoxOptions = new FirefoxOptions();
 		firefoxOptions.setHeadless(true);
-		//firefoxOptions.addArguments("--display=99");
 		driver = new FirefoxDriver(firefoxOptions);
 		driver.manage().timeouts().pageLoadTimeout(TIEMPO_MAXIMO_ESPERA, TimeUnit.SECONDS);
 	}
@@ -32,9 +36,23 @@ public class DriverUv {
 	/** realiza el login del usuario. */
 	public static void login() {
 		driver.get("http://localhost:8888/srv/es/index");
+		capturaPantalla("index");
 		WebElement username = driver.findElement(By.name("usuario"));
 		username.sendKeys("usig");
 		username.submit();
+	}
+	
+	/** realiza una captura de pantalla.
+	 * @param nombre nombre del fichero de la captura
+	 */
+	public static void capturaPantalla(String nombre) {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+	    String directorio = "/tmp/";
+	    try {
+			Files.copy(scrFile.toPath(), (new File(directorio + nombre + ".jpg")).toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static WebDriver getDriver() {
