64f3d8bde97cce1b201c6dc864d4de7e7d07f290
brodriguez <brodriguez@atisoluciones.com>
2021-06-08 12:41:35 +0200

    0.17 Sección Mis Méritos - Ordenación de items por código. - Tamaño de paginación a 50 por defecto. - Filtro de código de item con el formato: I.1.1
    Sección Mis Solicitudes, asignación de méritos a la bolsa seleccionada
    - Ordenación de items por código.
    - Tamaño de paginación a 50 por defecto.
    - Filtro de código de item con el formato: I.1.1
    
    Sessión Mis Solicitudes, resumen de solicitud
    - Ordenación de items por código.
    
    Pdf solicitud:
    - Ordenación de items por código.- Añadida la hora de confirmación de solicitud. (antes estaba a cero)
    - Añadido dni y dirección del candidato
    
    Convocatoria:
    - Añadida la hora final de cierre de la convocatoria a: 23:59:59

--- src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
+++ src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
@@ -19,7 +19,7 @@ import es.ujaen.uvirtual.utilidades.UVException;
  * @author ATISoluciones
  */
 public class ModeloParametrosConfiguracion {
-	private static final String VERSION = "0.16";
+	private static final String VERSION = "0.17";
 	protected static ModeloParametrosConfiguracion eInstancia;
 
 	/**
