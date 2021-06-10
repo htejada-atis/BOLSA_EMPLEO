ea353b88b1c57f15e20ddbff4d1b3fd8f2e6607b
brodriguez <brodriguez@atisoluciones.com>
2021-06-10 13:13:01 +0200

    0.23 - chequeo longitud de nombre de titulación - fix borrar mérito

--- src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
+++ src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
@@ -19,7 +19,7 @@ import es.ujaen.uvirtual.utilidades.UVException;
  * @author ATISoluciones
  */
 public class ModeloParametrosConfiguracion {
-	private static final String VERSION = "0.22";
+	private static final String VERSION = "0.23";
 	protected static ModeloParametrosConfiguracion eInstancia;
 
 	/**
