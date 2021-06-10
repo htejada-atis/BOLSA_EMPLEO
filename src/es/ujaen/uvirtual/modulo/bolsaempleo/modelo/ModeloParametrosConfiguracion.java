28d805dad9484c4049858879fbd22b79932334ae
brodriguez <brodriguez@atisoluciones.com>
2021-06-10 18:25:24 +0200

    0.25

--- src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
+++ src/es/ujaen/uvirtual/modulo/bolsaempleo/modelo/ModeloParametrosConfiguracion.java
@@ -19,7 +19,7 @@ import es.ujaen.uvirtual.utilidades.UVException;
  * @author ATISoluciones
  */
 public class ModeloParametrosConfiguracion {
-	private static final String VERSION = "0.24";
+	private static final String VERSION = "0.25";
 	protected static ModeloParametrosConfiguracion eInstancia;
 
 	/**
